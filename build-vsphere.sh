6046afe8478bd12ab3ee85cac173f719d360dfe6
Claudio Bantaloukas <cbantaloukas@devmac08.ccdc.cam.ac.uk>
2020-02-05 19:30:00 +0000

    Secure vagrant user when deploying in vmware

--- build-vsphere.sh
+++ build-vsphere.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 pushd $DIR
+
 if [[ ! -e ./vsphere-environment-do-not-add ]]
 then
   echo "Please add a vsphere-environment-do-not-add file to set up the environment variables required to deploy"
@@ -10,15 +10,16 @@ then
   return 1
 fi
 source ./vsphere-environment-do-not-add
+
 echo 'creating output directory'
 mkdir -p output
-
 rm -rf ./output//packer-centos-7.7-x86_64-vmware
 
 echo 'building base images'
 packer build \
   -only=vmware-iso \
   -except=vagrant,vsphere-template \
+  -var 'customise_for_buildmachine=1' \
   -var 'build_directory=./output/' \
   -var 'disk_size=400000' \
   -var 'cpus=2' \
